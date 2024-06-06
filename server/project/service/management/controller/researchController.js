319572405b4104a96e39386807d598114b13e95a
nattawatWork <natseakoo.work@gmail.com>
2024-06-07 01:02:40 +0700

    update researchController

--- server/project/service/management/controller/researchController.js
+++ server/project/service/management/controller/researchController.js
@@ -1,80 +1,67 @@
 var mongo = require('mongodb');
 var researchModel = require('../models/researchModel');
 
-exports.onQuery = async function (query) {
+exports.getResearchController = async function (query) {
     return new Promise((resolve, reject) => {
         researchModel
-            .findOne(query)
-            // .sort("coin")
-            .populate([
-                // {path : "address.province"},
-                // {path : "address.district"},
-                // {path : "bankInfo.bankName"}
-            ])
-            .lean()
-            .exec().then(doc => {
-                resolve(doc);
-            }).catch(err =>{
-                reject(err);
+            .findOne(query, { __v: 0 })
+            .then(doc => {
+                if (doc == null) {
+                    var resInfo = { result: {}, code: { codeNo: 404, description: 40401 } };
+                    reject(resInfo);
+                }
+                var resInfo = { result: doc, code: { codeNo: 200, description: 20000 } };
+                resolve(resInfo);
+            }).catch(err => {
+                var resInfo = { error: err, code: { codeNo: 500, description: 50003 } };
+                reject(resInfo);
             });
     });
 }
-exports.onQuerys = async function (query) {
+exports.getsResearchController = async function () {
     return new Promise((resolve, reject) => {
         researchModel
-            .find(query)
-            .sort({_id:-1})
-            .populate([
-                // {path : "address.province"},
-                // {path : "address.district"},
-                // {path : "bankInfo.bankName"}
-            ])
-            .lean()
-            .exec().then(doc => {
-                resolve(doc);
-            }).catch(err =>{
-                reject(err);
+            .find({}, { __v: 0 })
+            .sort({ _id: -1 })
+            .then(doc => {
+                var resInfo = { result: doc, code: { codeNo: 200, description: 20000 } };
+                resolve(resInfo);
+            }).catch(err => {
+                var resInfo = { error: err, code: { codeNo: 500, description: 50003 } };
+                reject(resInfo);
             });
     });
 }
-exports.onCreate = async function (data) {
+exports.addResearchController = async function (data) {
     return new Promise((resolve, reject) => {
-        var objSchemas = new researchModel(data);
-        objSchemas.save()
-        .then(doc => {
-            resolve(doc);
-        }).catch(err =>{
-            reject(err);
-        });
-    });
-}
-exports.onUpdate = async function (query,data) {
-    return new Promise((resolve, reject) => {
-        researchModel
-            .findOneAndUpdate(query, data, { new: true, returnOriginal: false, upsert: true })
-            .populate([
-                // {path : "address.province"},
-                // {path : "address.district"},
-                // {path : "bankInfo.bankName"}
-            ])
-            .lean()
-            .exec().then(doc => {
-                resolve(doc);
-            }).catch(err =>{
-                reject(err);
+        var researchModels = new researchModel(data);
+        researchModels.save()
+            .then(() => {
+                var resInfo = { result: {}, code: { codeNo: 200, description: 20000 } };
+                resolve(resInfo);
+            }).catch(err => {
+                var resInfo = { error: err, code: { codeNo: 500, description: 50003 } };
+                reject(resInfo);
             });
     });
 }
-exports.onDelete = async function (query) {
 
+exports.deleteResearchController = async function (query) {
     return new Promise((resolve, reject) => {
         researchModel
-            .remove(query)
-            .lean()
-            .exec().then(doc => {
-                resolve(doc);
-            }).catch(err =>{
-                reject(err);
+            .findOneAndDelete(query)
+            .then(ResearchDoc => {
+                // find user and delete
+                if (ResearchDoc) {
+                    var resInfo = { result: {}, code: { codeNo: 200, description: 20000 } }
+                    resolve(resInfo);
+                }
+                //no user in database
+                else {
+                    reject({ error: {}, code: { codeNo: 404, description: 40402 } });
+                }
+            }).catch(err => {
+                reject({ error: err, code: { codeNo: 500, description: 50000 } });
             });
     });
 }
