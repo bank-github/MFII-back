040f1e852787cc83598200034871eea404346a75
nattawatWork <natseakoo.work@gmail.com>
2024-07-08 15:24:30 +0700

    edit middleware

--- server/routes/app.routes.js
+++ server/routes/app.routes.js
@@ -90,6 +90,9 @@ var middleware = require("../../helpers/middleware");
   //research
   //user
   //================================================\\
+  
+  // csv download 
+  app.get('/download', middleware.verifyTokenAndRole("admin"),middleware.downloadCsv);
 
 };
 
