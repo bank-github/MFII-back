f7b0b40a6220ebd0ac051c55d3c01780916b2aa1
Jirateep011 <6431501011@lamduan.mfu.ac.th>
2024-07-10 21:00:17 +0700

    add newsModel-linkPage

--- server/project/service/management/models/newsModel.js
+++ server/project/service/management/models/newsModel.js
@@ -6,7 +6,8 @@ var Schema = mongoose.Schema;
 var newsModel = new Schema({
     filePath            : { type: Array, required: true },
     linkVideo           : { type: Array, required: true },
-    linkImage           : { type: Array, required: true }
+    linkImage           : { type: Array, required: true },
+    linkPage            : { type: String, required: true }
 
 });
 
